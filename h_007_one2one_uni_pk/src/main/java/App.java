7d7a444a10298896ee0b40822254cddb828a3804
HMM999 <527799918@qq.com>
2021-04-28 20:32:28 +0800

    2021/04/28

---
+++ h_007_one2one_uni_pk/src/main/java/App.java
@@ -0,0 +1,21 @@
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.tool.schema.TargetType;
+import org.junit.Test;
+
+import java.util.EnumSet;
+
+public class App {
+
+    @Test
+    public void test01() {
+        ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        Metadata metadata = new MetadataSources(registry).buildMetadata();
+        SchemaExport export = new SchemaExport();
+        export.create(EnumSet.of(TargetType.DATABASE), metadata);
+    }
+
+}
