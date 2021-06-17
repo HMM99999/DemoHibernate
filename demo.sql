12d3da91930873ec17b2df3e4f9be17d40b104e4
HHH <527799918@qq.com>
2021-06-17 14:49:50 +0800

    2021/06/17

--- demo.sql
+++ demo.sql
@@ -6,31 +6,10 @@ drop database hibernate;
 
 use hibernate;
 
-
 show tables;
 
 SELECT concat('DROP TABLE IF EXISTS ', table_name, ';') FROM information_schema.tables WHERE table_schema = 'hibernate';
 
-DROP TABLE IF EXISTS student;DROP TABLE IF EXISTS stuidcard;
+show tables;
 
-drop table hibernate_sequence;
-drop table stuidcard;
-drop table student;
-drop table husband;
-drop table wife;
-drop table t_user;
-drop table t_group;
-drop table t_s;
-drop table t_student;
-drop table t_teacher;
-
-select * from teacher for update ;
-
-
-select * from husband;
-select * from wife;
-select * from student;
-select * from stuidcard;
-
-select * from t_group;
-select * from t_user;
+select * from t_student
